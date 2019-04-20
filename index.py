eb9ca2ccf672fedd29046d733163641afe30b819
Jeff Pignataro <jeff.pignataro@sa.ucsb.edu>
2019-04-20 11:45:04 -0600

    Testing api gateway qs param logic

--- index.py
+++ index.py
@@ -1,8 +1,12 @@
 import json
 
+message = ""
+
 
 def handler(event, context):
-    message = event['method']
+    queryStringParameters = event['queryStringParameters'] or {}
+    if queryStringParameters:
+        message = queryStringParameters['method']
     returnVal = {
         "statusCode": 200,
         "body": json.dumps(message)
