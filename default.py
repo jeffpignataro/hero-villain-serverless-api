d32abcf590ef0bb529c7cfd25d1c88c06893bec7
Jeff Pignataro <jeff.pignataro@sa.ucsb.edu>
2019-04-19 18:12:05 -0600

    Add event handler for method calls.

--- default.py
+++ default.py
@@ -2,7 +2,8 @@ import json
 
 
 def handler(event, context):
+    message = event['method']
     return {
         "statusCode": 200,
-        "body": json.dumps('Cheers from AWS Lambda!!')
+        "body": json.dumps(message)
     }
