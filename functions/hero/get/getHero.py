76a6583028e287ccdd324ec17d8160688801a0cc
Jeff Pignataro <jeff.pignataro@sa.ucsb.edu>
2019-04-22 17:09:02 -0600

    Adding getAllHeroes method

--- functions/hero/get/getHero.py
+++ functions/hero/get/getHero.py
@@ -4,12 +4,20 @@ from models.villain.villain import Villain
 from helpers.data import getJsonFromFile, getJsonObjectById
 
 
+def getAllHeroes():
+    return GetHeroData()
+
+
 def getHeroById(id):
-    data = getJsonFromFile("heros.json")
-    heroJson = getJsonObjectById(data, id)
+    heroJson = getJsonObjectById(GetHeroData(), id)
     person = Person(heroJson['name'], heroJson['characters'])
     hero = Hero(person,
                 heroJson['alias'],
                 heroJson['publisher'],
                 Villain(Person('Name Stub', 'Character Stub'), 'Alias Stub', heroJson['publisher'], 'Crimes Stub'))  # stub for getVillainbyId()
     return hero.alias
+
+
+# Helper functions
+def GetHeroData():
+    return getJsonFromFile("heros.json")
